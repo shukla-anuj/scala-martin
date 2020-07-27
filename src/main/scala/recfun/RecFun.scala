2200347afa7f683bc6c4278a9ed3f800b54cc4c2
shukla-anuj <anujshukla4585@gmail.com>
2020-07-28 01:52:16 +0530

    scala project>> first checking

---
+++ src/main/scala/recfun/RecFun.scala
@@ -0,0 +1,56 @@
+package recfun
+
+import scala.collection.mutable.ListBuffer
+
+object RecFun extends RecFunInterface {
+
+  def main(args: Array[String]): Unit = {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(s"${pascal(col, row)} ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 0 || c == r) 1 else pascal(c-1,r-1) + pascal(c,r-1)
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def f(chars: List[Char], numOpens: Int): Boolean = {
+      if (chars.isEmpty) {
+        numOpens == 0
+      } else {
+        val h = chars.head
+        val n =
+          if (h == '(') numOpens + 1
+          else if (h == ')') numOpens - 1
+          else numOpens
+        if (n >= 0) f(chars.tail, n)
+        else false
+      }
+    }
+
+    f(chars, 0)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def count(m: Int, c: List[Int]) : Int = {
+      if (c.isEmpty) 0
+      else if (m - c.head == 0) 1
+      else if (m - c.head < 0) 0
+      else countChange(m - c.head, c) + countChange(m, c.tail)
+    }
+    count(money, coins.sorted)
+  }
+}
